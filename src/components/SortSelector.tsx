246570df3b6277fb3c4d3747497a66320eda0198
Lion <lion011095@gmail.com>
2023-06-20 14:25:42 -0400

    Sorting games implemented

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,19 +1,38 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { IoIosArrowDropdown } from "react-icons/io";
 
-const SortSelector = () => {
+interface SortSelectorProps {
+  onSelectSort: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSort, sortOrder }: SortSelectorProps) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release Date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average rating" },
+  ];
+
+  const currentSort = sortOrders.find((sort) => sort.value === sortOrder);
+
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<IoIosArrowDropdown />}>
-        Order by: Relevance
+        Order by: {currentSort?.label || "Relevance"}
       </MenuButton>
       <MenuList>
-        <MenuItem>Relevance</MenuItem>
-        <MenuItem>Release date</MenuItem>
-        <MenuItem>Date added</MenuItem>
-        <MenuItem>Name</MenuItem>
-        <MenuItem>Popularity</MenuItem>
-        <MenuItem>Average rating</MenuItem>
+        {sortOrders.map((order, index) => (
+          <MenuItem
+            onClick={() => onSelectSort(order.value)}
+            value={order.value}
+            key={index}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
